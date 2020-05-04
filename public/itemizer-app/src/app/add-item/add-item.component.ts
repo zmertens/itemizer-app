0d8dcbae68006fd24ce8b7f1ec47e4901de7dac8
zmertens <zmertens@asu.edu>
2020-05-03 18:38:09 -0700

    Setup item list, add item model and views \ Add Bootstrap to angular styles

--- public/itemizer-app/src/app/add-item/add-item.component.ts
+++ public/itemizer-app/src/app/add-item/add-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-item',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddItemComponent implements OnInit {
 
+  editMode: Boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(f: NgForm) {
+    if (f.invalid) {
+      console.error(f)
+    }
+  }
+
+  onClear() {
+
+  }
+
+  onDelete() {
+
   }
 
 }
--- public/itemizer-app/src/app/header/header.component.ts
+++ public/itemizer-app/src/app/header/header.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  isAuthenticated: Boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
