32db31458106fa05b312ab6c149c3766ea812563
zmertens <zmertens@asu.edu>
2020-05-28 08:24:51 -0700

    Update CORS option on backend to allow Angular base-href

--- source/itemizer-app.js
+++ source/itemizer-app.js
@@ -1,17 +1,20 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
 // Require db/mongoose so tests can have access
-require('./db/mongoose')
-const userRouter = require('./routers/user')
-const itemRouter = require('./routers/item')
+require("./db/mongoose");
+const userRouter = require("./routers/user");
+const itemRouter = require("./routers/item");
 
-const itemizerApp = express()
+const itemizerApp = express();
 
-itemizerApp.use(express.json())
-let corsOptions = { origin: 'http://localhost:4200', credentials: false }
-itemizerApp.use(cors(corsOptions))
-itemizerApp.options('*', cors())
-itemizerApp.use(userRouter)
-itemizerApp.use(itemRouter)
+itemizerApp.use(express.json());
+let corsOptions = {
+  origin: ["http://localhost:4200", "https://zmertens.github.io/itemizer-app"],
+  credentials: false,
+};
+itemizerApp.use(cors(corsOptions));
+itemizerApp.options("*", cors());
+itemizerApp.use(userRouter);
+itemizerApp.use(itemRouter);
 
-module.exports = itemizerApp
+module.exports = itemizerApp;
