31f7d491a3095c99202254b419f6e7a93199bb16
zmertens <zmertens@asu.edu>
2023-01-07 17:26:57 -0700

    Add more messegeService calls, update styling

--- public/itemizer-app/src/app/user/user.service.ts
+++ public/itemizer-app/src/app/user/user.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, throwError } from 'rxjs';
 import { map, tap, catchError } from 'rxjs/operators';
 import { User } from './user.model';
-import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+} from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { ItemService } from '../items/item.service';
 import { MessegeService } from '../messege.service';
@@ -10,62 +14,88 @@ import { MessegeService } from '../messege.service';
   providedIn: 'root',
 })
 export class UserService {
-
   user = new BehaviorSubject<User>(null);
 
   readonly url = environment.serverUrl;
 
-  constructor(private http: HttpClient, private itemService: ItemService,
-    private messegeService: MessegeService) {}
+  constructor(
+    private http: HttpClient,
+    private itemService: ItemService,
+    private messegeService: MessegeService
+  ) {}
 
   /**
    * When creating a new user, post an <any> object - The client User model
    *  does not have a password key which is what the backend expects.
    * @param name
-   * @param userEmail 
-   * @param password 
+   * @param userEmail
+   * @param password
    */
   createUser(
     name: String,
     userEmail: String,
     password: String
   ): Observable<User> {
-    return this.http.post<any>(
-      this.url + '/users',
-      { name: name, email: userEmail, password: password}
-    ).pipe(catchError(this.handleError), tap(responseData =>
-      this.handleAuthentication(responseData.name, responseData.email, responseData.token)
-    ));
+    return this.http
+      .post<any>(this.url + '/users', {
+        name: name,
+        email: userEmail,
+        password: password,
+      })
+      .pipe(
+        catchError(this.handleError),
+        tap((responseData) =>
+          this.handleAuthentication(
+            responseData.name,
+            responseData.email,
+            responseData.token
+          )
+        )
+      );
   }
 
   loginUser(userEmail, password): Observable<User> {
-    return this.http.post<User>(this.url + '/users/login', {
-      email: userEmail,
-      password: password,
-    }).pipe(catchError(this.handleError), tap(responseData =>
-      this.handleAuthentication(responseData.name, responseData.email, responseData.token)
-    ));
+    return this.http
+      .post<User>(this.url + '/users/login', {
+        email: userEmail,
+        password: password,
+      })
+      .pipe(
+        catchError(this.handleError),
+        tap((responseData) => {
+          this.handleAuthentication(
+            responseData.name,
+            responseData.email,
+            responseData.token
+          );
+          if (environment.production !== true) {
+            this.messegeService.addMessage(
+              `Loggin user w/ email: ${userEmail}, password: ${password}\n`
+            );
+          }
+        })
+      );
   }
 
   logoutUser(): Observable<void | User> {
-    return this.http
-      .post<User>(this.url + '/users/logout', {})
-      .pipe(
-        tap((responseData) => {
-          this.itemService.items = [];
-          this.user.next(null);
-          localStorage.setItem('authToken', '');
-        }),
-        catchError((err) => {
-          throw new Error('Failed to log out user: ' + err);
-        })
-      );
+    return this.http.post<User>(this.url + '/users/logout', {}).pipe(
+      tap((responseData) => {
+        if (environment.production !== true) {
+          this.messegeService.addMessage(`Logout user: ${JSON.stringify(this.user.value)}`);
+        }
+        this.itemService.items = [];
+        this.user.next(null);
+        localStorage.setItem('authToken', '');
+      }),
+      catchError((err) => {
+        throw new Error('Failed to log out user: ' + err);
+      })
+    );
   }
 
   private handleAuthentication(name: String, email: String, token: String) {
     let newUser = new User(name, email, token);
     this.user.next(newUser);
-    this.messegeService.addMessage(`Loggin user: ${name}, email: ${email}\n`);
     localStorage.setItem('authToken', token.toString());
   }
 
@@ -74,6 +104,6 @@ export class UserService {
     if (!err.error || !err.error.error) {
       return throwError(errorMessage);
     }
-    return throwError(errorMessage)
+    return throwError(errorMessage);
   }
 }
